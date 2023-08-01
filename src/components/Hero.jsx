b5ee584dd921a8c5b81746654d91a2cb9ab87258
nickhitos <nickhit3802@gmail.com>
2023-07-31 21:25:18 -0700

    Added new logo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,8 +6,8 @@ const Hero = () => {
 			<nav className="flex justify-between items-center w-full mb-10 pt-3">
 				<img
 					src={logo}
-					alt="sumz_logo"
-					className="w-28 object-contain"
+					alt="summarigo_logo"
+					className="w-29 object-contain"
 				/>
 				<button
 					type="button"
